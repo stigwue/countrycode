89c1e0b4f7c8636e6813e803c824c8344117d178
Stephen Igwue <olibenu@yahoo.com>
2016-04-09 22:51:48 +0100

    Country name wrap with quotes

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 #function to write data to file
 def write_to_file(filename, line):
     f = open(filename, 'ab')
+    #open(filename, 'ab')
 
     f.write(line)
     f.write('\n')
@@ -34,35 +35,38 @@ with open('input/country-codes.csv', 'rb') as f:
 		col3 = row[14]
 		col4 = row[17]
 		#print row
-		#print '%s,%s,%s,%s,%s' % (col0, col1, col2, col3, col4)
+		print '"%s",%s,%s,%s,%s' % (col0, col1, col2, col3, col4)
 
 		#send to csv output
-		#write_to_file('output/new.csv', '%s,%s,%s,%s,%s' % (col0, col1, col2, col3, col4))
+		write_to_file('output/csv.csv', '%s,%s,%s,%s,%s' % (col0, col1, col2, col3, col4))
 
 
 #get col 5 from
 #https://github.com/mhs/world-currencies/blob/master/currencies.json
 #or
 #http://fx.sauder.ubc.ca/currency_table.html
-import json
-f = open('input/currencies.json', 'r')
-json_string = ''
-line = f.readline()
-while line != '':
-	json_string = json_string + line
+# import json
+# f = open('input/currencies.json', 'rb')
+# json_string = ''
+# line = f.readline()
+# while line != '':
+# 	json_string = json_string + line
 
-	line = f.readline()
+# 	line = f.readline()
 
-currencies = json.loads(json_string) #cc/currency code, symbol, name
-for currency in currencies:
-	print currency['cc'], currency['symbol'] #, currency['name']
+# currencies = json.loads(json_string) #cc/currency code, symbol, name
+# for currency in currencies:
+# 	print currency['cc'], currency['symbol'] #, currency['name']
 
-	#send to csv output
-	#try:
-   		#write_to_file('output/cc.csv', '%s,%s' % (currency['cc'], currency['symbol']))
-   	#except:
-   		#write_to_file('output/cc.csv', '%s,%s' % (currency['cc'], currency['symbol'].encode('utf-8').strip()))
-	write_to_file('output/cc.csv', '%s,%s' % (currency['cc'], u''.join(('',currency['symbol'])).encode('utf-8').strip()))
+# 	#send to csv output
+# 	try:
+#    		write_to_file('output/cc.csv', '%s,%s' % (currency['cc'], currency['symbol']))
+#    	except:
+#    		write_to_file('output/cc.csv', '%s,%s' % (currency['cc'], currency['symbol'].encode('utf-8').strip()))
+	
+	#write_to_file('output/cc.csv', '%s,%s' % (currency['cc'], u''.join(('',currency['symbol'])).encode('utf-8').strip()))
+	#write_to_file('output/cc.csv', '%s,%s' % (currency['cc'], currency['symbol'].encode("ascii", "ignore").strip()))
+	#write_to_file('output/cc.csv', '%s,%s' % (currency['cc'], currency['symbol'].encode("ascii").strip()))
 
-
+	#write_to_file('output/cc.csv', '%s,%s' % (currency['cc'], unicode(currency['symbol'])))
 
